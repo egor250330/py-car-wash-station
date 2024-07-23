cc2cac306a704c1ac404d3f9a0315959c2e5ad2b
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-07-24 00:14:42 +0300

    add 4 method for automatic calculate price

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -26,8 +28,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            (car.comfort_class * (self.clean_power - car.clean_mark) *
-             self.average_rating) / self.distance_from_city_center,
+            (car.comfort_class * (self.clean_power - car.clean_mark)
+             * self.average_rating) / self.distance_from_city_center,
             1
         )
 
@@ -36,5 +38,6 @@ class CarWashStation:
 
     def rate_service(self, rating: float) -> None:
         self.count_of_ratings += 1
-        total_rating = self.average_rating * (self.count_of_ratings - 1) + rating
+        total_rating = (self.average_rating
+                        * (self.count_of_ratings - 1) + rating)
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
